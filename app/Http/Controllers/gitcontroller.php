6420715477f078bdf856154eea7ac91dcc4ff237
PiyushNagwani <agwanipiyush786@gmial.com>
2021-05-24 01:34:00 +0530

    initial upload of the project

---
+++ app/Http/Controllers/gitcontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class gitcontroller extends Controller
+{
+    //
+    function index()
+    {
+        echo"my git file is upload successfully";
+    }
+}
